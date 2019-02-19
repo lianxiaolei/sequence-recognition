8a0151c6f2801b3233d447ee94f1d743f5676044
连晓磊 <lianxiaolei@hqyxjy.com>
2019-02-19 23:31:52 +0800

    update: Change loop num

--- recognition/kernel/network.py
+++ recognition/kernel/network.py
@@ -342,8 +342,9 @@ class CRNN():
 
   def run(self):
     for epoch in range(2048):
+      inputs, sparse_targets, seq_len = get_next_batch(self.FLAGS.batch_size)
       for step in range(32):
-        inputs, sparse_targets, seq_len = get_next_batch(self.FLAGS.batch_size)
+        # inputs, sparse_targets, seq_len = get_next_batch(self.FLAGS.batch_size)
         # print('sequence length', seq_len)
         self.train_step(inputs, sparse_targets, seq_len)
         # current_step = tf.train.global_step(self.sess, self.global_step)
