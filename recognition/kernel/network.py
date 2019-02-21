279ce29b963bf062eee1ac96b2107b11eb22b168
连晓磊 <lianxiaolei@hqyxjy.com>
2019-02-21 18:11:34 +0800

    update: Change batch_size to 64

--- recognition/kernel/network.py
+++ recognition/kernel/network.py
@@ -383,7 +383,7 @@ if __name__ == '__main__':
   tf.app.flags.DEFINE_boolean("log_device_placement",
                               False, "Log placement of ops on devices")
   tf.app.flags.DEFINE_integer("batch_size",
-                              256, "Batch Size (default: 64)")
+                              64, "Batch Size (default: 64)")
   tf.app.flags.DEFINE_float("dropout_keep_prob",
                             0.75, "Dropout keep probability (default: 0.5)")
   tf.app.flags.DEFINE_integer("evaluate_every",
